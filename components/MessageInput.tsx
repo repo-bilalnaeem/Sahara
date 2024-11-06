9814047e8d4b89eafb10fdf9389d6c681f2baf01
Bilal Naeem <94391016+repo-bilalnaeem@users.noreply.github.com>
2024-11-06 12:23:45 +0500

    removing auto focus from SaharaBot

--- components/MessageInput.tsx
+++ components/MessageInput.tsx
@@ -112,7 +112,7 @@ const MessageInput = ({ onShouldSend }: Props) => {
         </Animated.View>
 
         <TextInput
-          autoFocus
+          // autoFocus
           ref={inputRef}
           placeholder="Message"
           style={styles.messageInput}
